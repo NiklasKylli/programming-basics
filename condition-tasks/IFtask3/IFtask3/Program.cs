028098fd052795fbea15ec2f994f57f821068530
NiklasKylli <Niklas.kylli@gmail.com>
2018-09-26 11:34:07 +0300

    IF task 3 done

--- condition-tasks/IFtask3/IFtask3/Program.cs
+++ condition-tasks/IFtask3/IFtask3/Program.cs
@@ -6,7 +6,27 @@ namespace IFtask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvittää onko luku negatiivinen vai positiivinen ja onko luku pariton vai parillinen");
+            Console.Write("Syötä numero");
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isNumber = int.TryParse(userInput, out number);
+
+            if (isNumber)
+                if (number == 0)
+                    Console.WriteLine("Luku on parillinen");
+
+                else if (number < 0)
+
+                    if (number % 2 == 0)
+                        Console.WriteLine($"Numero {number} on negatiivinen ja parillinen");
+                    else
+                        Console.WriteLine($"Numero {number} on negatiivinen ja pariton");
+                else
+                    if (number % 2 == 0)
+                    Console.WriteLine($"Numero {number} on positiivinen ja parillinen");
+                else
+                    Console.WriteLine($"Numero {number} on positiivinen ja pariton");
         }
     }
 }
