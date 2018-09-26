a9f033991ac874787a23b89272bc83a777750013
NiklasKylli <Niklas.kylli@gmail.com>
2018-09-26 11:06:18 +0300

    If task3

---
+++ condition-tasks/IFtask3/IFtask3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFtask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
