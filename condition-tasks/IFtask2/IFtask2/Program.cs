e4afb3ceb2ec007a57a691cf39996544c930aac6
NiklasKylli <Niklas.kylli@gmail.com>
2018-09-26 10:51:03 +0300

    IF task 2 done

--- condition-tasks/IFtask2/IFtask2/Program.cs
+++ condition-tasks/IFtask2/IFtask2/Program.cs
@@ -6,7 +6,20 @@ namespace IFtask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                
+            Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla");
+            Console.Write("Syötä numero");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"Numero {number} on nolla");
+            else if (number < 0)
+                Console.WriteLine($"Numero {number} on negatiivinen!");
+            else
+                Console.WriteLine($"Numero {number} on positviinen!");
         }
     }
 }
+    
+
