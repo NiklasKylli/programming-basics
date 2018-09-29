90e018bf2aea7e2fb8687a48c487aa92c4f4aa7e
NiklasKylli <Niklas.kylli@gmail.com>
2018-09-30 00:36:27 +0300

    Iftask 4

--- condition-tasks/IFtask4/IFtask4/Program.cs
+++ condition-tasks/IFtask4/IFtask4/Program.cs
@@ -6,7 +6,26 @@ namespace IFtask4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma lajittelee 3 lukua nousevaan järjestykseen");
+            Console.Write("Syötä 1. luku: ");
+            string userInput = Console.ReadLine();
+            int x = int.Parse(userInput);
+
+
+
+            Console.Write("Syötä 2. luku: ");
+            userInput = Console.ReadLine();
+            int y = int.Parse(userInput);
+
+            Console.Write("Syötä 3. luku: ");
+            userInput = Console.ReadLine();
+            int z = int.Parse(userInput);
+
+            
+            Console.WriteLine($"syötit luvut {x} - {y} - {z}");
+
+            
+            
         }
     }
 }
